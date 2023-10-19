a79d29348c67347c2bb38b5bc75f644df543466e
0147ntekim <147ntekim@gmail.com>
2023-10-19 09:40:13 +0100

    commit

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const closeNav = document.querySelector('.close-nav')
 const hamburger = document.querySelector('.hamburger')
 const navLinks = document.querySelector('.nav-links')
-const header = document.querySelector('.header')
 const logo = document.querySelector('.logo')
+const header = document.querySelector('.header')
+const navbar = document.querySelector('.navbar')
+
 
 hamburger.addEventListener('click', openNav)
 closeNav.addEventListener('click', closeNavBar)
@@ -12,7 +14,8 @@ function openNav(){
     hamburger.classList.add('close-hamburger')
     navLinks.classList.add('open-mobile-nav')
     logo.classList.add('open-logo-nav-link')
-    header.classList.add('.padding_out')
+    header.classList.add('padding_out')
+    navbar.classList.add('nav_out')
 }
 
 function closeNavBar(){
@@ -20,5 +23,6 @@ function closeNavBar(){
     closeNav.classList.remove('open-nav')
     navLinks.classList.remove('open-mobile-nav')
     logo.classList.remove('open-logo-nav-link')
-    header.classList.remove('.padding_out') 
+    header.classList.remove('padding_out') 
+    navbar.classList.remove('nav_out')
 }
